bbd9aa3f1d6c37aa4a5d47dd861d583a2ae01ae3
Filakcz <blahafila@gmail.com>
2026-05-08 12:08:03 +0200

    Importing activities

---
+++ DomestiqAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DomestiqAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
