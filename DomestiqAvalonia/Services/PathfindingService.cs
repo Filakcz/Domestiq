f9352e75e40d31932ad61e3b27897af843969017
Filakcz <blahafila@gmail.com>
2026-05-08 22:11:41 +0200

    Route planning

---
+++ DomestiqAvalonia/Services/PathfindingService.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using DomestiqAvalonia.Models;
+
+namespace DomestiqAvalonia.Services;
+
+public class PathfindingService
+{
+    // dijsktra, A*
+    public List<RouteNode> FindPath(RouteNode start, RouteNode end, List<RouteEdge> edges)
+    {
+        return new List<RouteNode> { start, end };
+    }
+
+    private double CalculateDistance(RouteNode a, RouteNode b)
+    {
+        return Math.Sqrt(Math.Pow(a.Latitude - b.Latitude, 2) + Math.Pow(a.Longitude - b.Longitude, 2));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Domestiq
-Your personal domestique that plans and adapts your training for peak perfomance.
+Your personal domestique that plans a route for you.
